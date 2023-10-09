76c51e8113889b963abd380c9165ff1707533d50
DiegoHS07 <dhiguera09@gmail.com>
2023-10-09 17:27:06 -0500

    Creación de varias piezas del tetris

--- main.js
+++ main.js
@@ -53,11 +53,55 @@ const piece = {
   position: { x: 5, y: 5 },
   shape: [
     [1, 1],
-    [1, 1]
+    [1, 1],
   ],
 };
 
-function update() {
+const PIECES = [
+  [
+    [1, 1],
+    [1, 1],
+  ],
+  [[1, 1, 1, 1]],
+  [
+    [0, 1, 0],
+    [1, 1, 1],
+  ],
+  [
+    [1, 1, 0],
+    [0, 1, 1],
+  ],
+  [
+    [1, 0],
+    [1, 0],
+    [1, 1],
+  ],
+];
+
+// function update() {
+//   draw();
+//   window.requestAnimationFrame(update);
+// }
+let dropCounter = 0;
+let lastTime = 0;
+
+function update(time = 0) {
+  const deltaTime = time - lastTime;
+  lastTime = time;
+
+  dropCounter += deltaTime;
+
+  if (dropCounter > 1000) {
+    piece.position.y++;
+    dropCounter = 0;
+
+    if (checkCollision()) {
+      piece.position.y--;
+      solidifyPiece();
+      removeRows();
+    }
+  }
+
   draw();
   window.requestAnimationFrame(update);
 }
@@ -72,80 +116,80 @@ function draw() {
         context.fillStyle = "yellow";
         context.fillRect(x, y, 1, 1);
       }
-    })
-  })
+    });
+  });
 
   piece.shape.forEach((row, y) => {
     row.forEach((value, x) => {
       if (value) {
-        context.fillStyle = "red"
-        context.fillRect(x + piece.position.x, y + piece.position.y, 1, 1)
+        context.fillStyle = "red";
+        context.fillRect(x + piece.position.x, y + piece.position.y, 1, 1);
       }
-    })
-  })
+    });
+  });
 }
 
-document.addEventListener('keydown', event => {
-  if (event.key === 'ArrowLeft') {
-    piece.position.x--
+document.addEventListener("keydown", (event) => {
+  if (event.key === "ArrowLeft") {
+    piece.position.x--;
     if (checkCollision()) {
-      piece.position.x++
+      piece.position.x++;
     }
   }
   if (event.key === "ArrowRight") {
-    piece.position.x++
+    piece.position.x++;
     if (checkCollision()) {
       piece.position.x--;
     }
   }
   if (event.key === "ArrowDown") {
-    piece.position.y++
+    piece.position.y++;
     if (checkCollision()) {
-      piece.position.y--
-      solidifyPiece()
-      removeRows()
+      piece.position.y--;
+      solidifyPiece();
+      removeRows();
     }
   }
-})
+});
 
-function checkCollision () {
+function checkCollision() {
   return piece.shape.find((row, y) => {
     return row.find((value, x) => {
       return (
-        value !== 0 &&
-        board[y + piece.position.y]?.[x + piece.position.x] !== 0
+        value !== 0 && board[y + piece.position.y]?.[x + piece.position.x] !== 0
       );
-    })
-  })
+    });
+  });
 }
 
-function solidifyPiece () {
+function solidifyPiece() {
   piece.shape.forEach((row, x) => {
     row.forEach((value, y) => {
       if (value === 1) {
-        board[y + piece.position.y][x + piece.position.x] = 1
+        board[y + piece.position.y][x + piece.position.x] = 1;
       }
-    })
-  })
+    });
+  });
 
-  piece.position.x = 0
-  piece.position.y = 0
+  piece.shape = PIECES[Math.floor(Math.random() * PIECES.length)]
+  piece.position.x = 0;
+  piece.position.y = 0;
 }
 
 function removeRows() {
-  const rowsToRemove = []
+  const rowsToRemove = [];
 
   board.forEach((row, y) => {
-    if (row.every(value => value === 1)) {
-      rowsToRemove.push(y)
+    if (row.every((value) => value === 1)) {
+      rowsToRemove.push(y);
     }
-  })
+  });
 
-  rowsToRemove.forEach(y => {
-    board.splice(y, 1)
-    const newRow = Array(BOARD_WIDTH).fill(0)
-    board.unshift(newRow)
-  })
+  rowsToRemove.forEach((y) => {
+    board.splice(y, 1);
+    const newRow = Array(BOARD_WIDTH).fill(0);
+    board.unshift(newRow);
+  });
 }
 
 update();
